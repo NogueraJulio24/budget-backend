991cf459e0e5ab5d3b2098c25ff32b7dfdcd2e8f
Julio Noguera <noguerajulio24@gmail.com>
2020-05-30 13:08:41 -0500

    add transaction_controller

--- app/controllers/api/v1/web/api_v1_application_controller.rb
+++ app/controllers/api/v1/web/api_v1_application_controller.rb
@@ -1,7 +1,6 @@
 module Api
     module V1
-        class ApiV1ApplicationController < ApplicationController
-            
+        class ApiV1ApplicationController < ActionController::Base
             http_basic_authenticable_with name:"name", password: "hola", if: Proc.new{Rails.env.production?}
         
         end
